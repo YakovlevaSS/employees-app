efb0fb42001957dd6eedf8d7b184e93e8cd0a2ce
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-03-14 21:29:12 +0300

    create employees

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/user', require('./routes/users'));
+app.use('/api/employees', require("./routes/employees"));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

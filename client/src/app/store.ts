6d6c9a4953849a469c983cf79a38af6f31b41a12
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-03-18 12:37:42 +0300

    запрос к апи

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,11 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { api } from "./services/api";
 import auth from '../features/auth/authSlice'
 
+
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(api.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,20 +1,44 @@
 import { Card, Form, Row, Space, Typography } from "antd";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { CustomInput } from "../../components/custom-input/input";
 import { Layout } from "../../components/layout";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
 import { Paths } from "../../paths";
+import { UserData, useLoginMutation } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+
+      navigate("/");
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError("Неизвестная ошибка");
+      }
+  }
+  }
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Войдите" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
-            <CustomButton type="primary" htmlType="submit">
+            <CustomButton type="primary" htmlType="submit" loading={loginUserResult.isLoading}>
               Войти
             </CustomButton>
           </Form>
@@ -23,6 +47,7 @@ export const Login = () => {
             <Typography.Text>
               Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
             </Typography.Text>
+            <ErrorMessage message={error}/>
           </Space>
         </Card>
       </Row>

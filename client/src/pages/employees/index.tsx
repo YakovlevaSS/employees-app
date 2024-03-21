7a48c5270a59b1d50992634ecdb6e5b4f6595143
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-03-21 16:50:38 +0300

    add employee page

--- client/src/pages/employees/index.tsx
+++ client/src/pages/employees/index.tsx
@@ -40,9 +40,11 @@ useEffect(() => {
   }
 }, [user, navigate]);
 
+const gotToAddUser = () => navigate(Paths.employeeAdd);
+
   return (
     <Layout>
-     <CustomButton type="primary" onClick={() => null} icon={<PlusCircleOutlined/>}>
+     <CustomButton type="primary" onClick={gotToAddUser} icon={<PlusCircleOutlined/>}>
         Добавить
     </CustomButton> 
     <Table
